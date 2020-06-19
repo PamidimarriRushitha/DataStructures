e8ef7e3e4de844a7f8e1200ca89fbfe931ade41b
PamidimarriRushitha <52276540+PamidimarriRushitha@users.noreply.github.com>
2020-06-19 17:42:26 +0530

    Create nextgreatest.py

---
+++ nextgreatest.py
@@ -0,0 +1,16 @@
+#next greatest number with these digits
+
+from itertools import permutations
+s=str(input())
+l=list(s)
+m=list(permutations(l,len(s)))
+r=[]
+for i in range(len(m)):
+    t=list(m[i])
+    t="".join(t)
+    if t>s:
+        r.append(t)
+if len(r)>0:
+    print(min(r))
+else:
+    print(-1)
