42813ab63bbb037dee1e0b6a99fe810ab539038d
PamidimarriRushitha <52276540+PamidimarriRushitha@users.noreply.github.com>
2020-06-19 18:18:29 +0530

    Create alternate.py

---
+++ alternate.py
@@ -0,0 +1,12 @@
+n=int(input())
+l=[int(x) for x in input().split()]
+a=[]
+for i in range(n):
+    if i%2==0:
+        a.append(max(l))
+        l.remove(max(l))
+    else:
+        a.append(min(l))
+        l.remove(min(l))
+print(*a)
+#maximum next minimum
